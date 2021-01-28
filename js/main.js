8ef5622a6a9989aa4ccca8cf3d82a95ef1633162
Shawn Kebel <fxrender@outlook.com>
2021-01-28 10:54:54 -0600

    Add video and 'play button functionality pt-1'

---
+++ js/main.js
@@ -0,0 +1,9 @@
+
+
+$(document).ready(function() {
+  /* -------------------------- Video Popup ------------------------------- */
+  const videoSrc = $("#player-1").attr("src");
+  $(".video-play-btn, .video-popup").on("click", function() {
+      alert("hi");
+  });  
+});  
