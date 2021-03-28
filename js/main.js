7e6d43bf64924a122098b69afbe1a9935da9c094
Shawn Kebel <fxrender@outlook.com>
2021-03-28 09:24:08 -0500

    Add 'light/dark' option using local storage with jQuery

--- js/main.js
+++ js/main.js
@@ -107,4 +107,37 @@ $(document).ready(function () {
   $(".nav-link").on("click", function () {
     $(".navbar-collapse").collapse("hide");
   });
+
+  /* --------------------------------- Toggle Theme - Light & Dark Mode --------------------------- */
+  function toggleTheme() {
+    if (localStorage.getItem("kbl-theme") !== null) {
+      if (localStorage.getItem("kbl-theme") === "dark") {
+        $("body").addClass("dark");
+      } else {
+        $("body").removeClass("dark");
+      }
+    }
+    updateIcon();
+  }
+  toggleTheme();
+
+  $(".toggle-theme").on("click", function () {
+    $("body").toggleClass("dark");
+    if ($("body").hasClass("dark")) {
+      localStorage.setItem("kbl-theme", "dark");
+    } else {
+      localStorage.setItem("kbl-theme", "light");
+    }
+    updateIcon();
+  });
+
+  function updateIcon() {
+    if ($("body").hasClass("dark")) {
+      $(".toggle-theme i").removeClass("fa-moon");
+      $(".toggle-theme i").addClass("fa-sun");
+    } else {
+      $(".toggle-theme i").removeClass("fa-sun");
+      $(".toggle-theme i").addClass("fa-moon");
+    }
+  }
 });
